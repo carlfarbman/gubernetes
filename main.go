faa89d12f16d90b93f7a3526b4d560e500596852
tdelph <tdelph@newrelic.com>
2024-05-21 10:55:26 -0700

    beautify

--- main.go
+++ main.go
@@ -15,9 +15,25 @@ func main() {
 	// Grab the argument from the command line.
 	arg := os.Args[1]
 
-	// Convert the string into a slice of runes
+	// Convert the string into a slice of runes.
 	runes := []rune(arg)
 
-	// Print the required output: word and the number of characters (as runes).
-	fmt.Printf("Word: %s\nChars: %d\n", arg, len(runes))
+	// Check if the word has more than 2 characters.
+	// If so, create an abbreviation by using the first and last character
+	// with the rune count in between.
+	// Otherwise, the abbreviation will be the word itself.
+	var abbreviation string
+	if len(runes) > 2 {
+		abbreviation = fmt.Sprintf("%c%d%c", runes[0], len(runes)-2, runes[len(runes)-1])
+	} else {
+		abbreviation = arg
+	}
+
+	startBold := "\033[1m"
+	// ANSI escape code to reset all formatting
+	resetFormat := "\033[0m"
+
+	// Print the bold abbreviation.
+	// Wrap the abbreviation with the ANSI codes for bold text.
+	fmt.Printf("%s: %s%s%s\n", arg, startBold, abbreviation, resetFormat)
 }
